b207c8135700b10557319b33eb47528e6d2ad187
yelbasheer <jodeveloper8@gmail.com>
2026-02-12 11:51:54 +0300

    fix:add unique to template columns

--- database/migrations/2024_01_01_000000_create_notification_templates_table.php
+++ database/migrations/2024_01_01_000000_create_notification_templates_table.php
@@ -8,19 +8,18 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('notification_templates', function (Blueprint $table) {
+        Schema::create('notification_templates', static function (Blueprint $table) {
             $table->id();
             $table->string("model");
             $table->string("key");
-            $table->string("lang")
-                ->default("ar");
+            $table->string("lang")->default("ar");
             $table->string("channel");
             $table->text("template");
-            $table->boolean("with_file")
-                ->default(false);
-            $table->json("prob")
-                ->nullable();
+            $table->boolean("with_file")->default(false);
+            $table->json("prob")->nullable();
             $table->timestamps();
+
+            $table->unique(["model","lang", "key", "channel"],'templates_unique_keys');
         });
     }
 };
